7eb2a2a4df53c23d5ec080090cd6e93d2825a4bd
vath.tr-dev <vthien562004@gmail.com>
2024-12-07 13:34:07 +0700

    Update authMiddleware.js

--- bt_node5/middleware/authMiddleware.js
+++ bt_node5/middleware/authMiddleware.js
@@ -1,16 +1,20 @@
 import jwt from "jsonwebtoken";
 
-export const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Lấy token từ header
-  if (!token) {
-    return res.status(401).json({ message: "Token không được cung cấp!" });
-  }
+const authMiddleware = {
+  verifyToken: (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1]; 
+    if (!token) {
+      return res.status(401).json({ message: "Token không được cung cấp!" });
+    }
 
-  try {
-    const decoded = jwt.verify(token, "secret"); // "secret" là khóa bí mật, thay đổi nếu cần
-    req.user = decoded; // Lưu thông tin người dùng vào request
-    next();
-  } catch (error) {
-    return res.status(403).json({ message: "Token không hợp lệ!" });
-  }
+    try {
+      const decoded = jwt.verify(token, "secret"); 
+      req.user = decoded; 
+      next();
+    } catch (error) {
+      return res.status(403).json({ message: "Token không hợp lệ!" });
+    }
+  },
 };
+
+export default authMiddleware;
