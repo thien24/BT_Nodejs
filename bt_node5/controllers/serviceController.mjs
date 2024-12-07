7b01239983be11fb0f3c451a5e2b3f75bc562d04
vath.tr-dev <vthien562004@gmail.com>
2024-12-07 13:33:53 +0700

    Update serviceController.mjs

--- bt_node5/controllers/serviceController.mjs
+++ bt_node5/controllers/serviceController.mjs
@@ -1,10 +1,14 @@
-// const Service = require('../models/serviceModel');
 import Service from "../models/serviceModel.mjs";
-export const getServices = async (req, res) => {
-  try {
-    const services = await Service.find();
-    res.status(200).json(services);
-  } catch (error) {
-    res.status(400).json({ message: 'Không thể lấy danh sách dịch vụ', error: error.message });
-  }
+
+const serviceController = {
+  getServices: async (req, res) => {
+    try {
+      const services = await Service.find();
+      res.status(200).json(services);
+    } catch (error) {
+      res.status(400).json({ message: 'Không thể lấy danh sách dịch vụ', error: error.message });
+    }
+  },
 };
+
+export default serviceController;
